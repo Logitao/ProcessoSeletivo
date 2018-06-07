6b176e6925ebd0b46c06ef34a5a6bac79f079473
Logitao <gmdramos@gmail.com>
2018-06-07 20:09:20 -0300

    Add files via upload

---
+++ App_Code/Funcionario.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Funcionario
+/// </summary>
+public class Funcionario
+{
+
+    public static DataTable RetornarPorID(int id)
+    {
+
+        return clsDados
+            .Instance
+            .Query(@"select i_func_id ID,
+                    vc_func_nome Nome, 
+                    vc_func_funcional Funcional,
+                    vc_func_filial Filial
+                    from TB_FUNCIONARIOS WHERE i_func_id = @id", new SqlParameter("@id", id));
+    }
+
+    public static DataTable Retornar()
+    {
+        return clsDados
+            .Instance
+            .Query(@"select i_func_id ID,
+                    vc_func_nome Nome, 
+                    vc_func_funcional Funcional,
+                    vc_func_filial Filial
+                    from TB_FUNCIONARIOS");
+    }
+
+    public static DataTable RetornarPorFilial(string filial)
+    {
+        return clsDados
+             .Instance
+             .Query(@"select i_func_id ID,
+                    vc_func_nome Nome, 
+                    vc_func_funcional Funcional,
+                    vc_func_filial Filial
+                    from TB_FUNCIONARIOS WHERE vc_func_filial = @filial", new SqlParameter("@filial", filial));
+    }
+    public static DataTable RetornarFiliais()
+    {
+        return clsDados
+            .Instance
+            .Query("select distinct vc_func_filial Filial from TB_FUNCIONARIOS");
+    }
+}
---
+++ App_Code/clsDados.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+/// <summary>
+/// Summary description for clsDados
+/// </summary>
+public class clsDados
+{
+    public static clsDados Instance
+    {
+        get
+        {
+            return new clsDados();
+        }
+    }
+
+    public string StringConexao
+    {
+        get
+        {
+            //Web.config
+            return ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+        }
+    }
+
+    public SqlConnection Connection;
+
+    private clsDados()
+    {
+        Connection = new SqlConnection(StringConexao);
+    }
+
+    public bool OpenConnection()
+    {
+        if (Connection.State == ConnectionState.Closed)
+        {
+            Connection.Open();
+            return true;
+        }
+        else return false;
+    }
+
+    public bool CloseConnection()
+    {
+        if (Connection.State == ConnectionState.Open)
+        {
+            Connection.Close();
+
+            return true;
+        }
+        else return false;
+    }
+
+    public DataTable Query(string sql, List<SqlParameter> Parametros)
+    {
+        if (OpenConnection())
+        {
+            SqlCommand comando = new SqlCommand(sql, Connection);
+            comando.CommandType = CommandType.Text;
+
+            foreach (var parametro in Parametros)
+                comando.Parameters.Add(parametro);
+            
+
+
+            SqlDataAdapter da = new SqlDataAdapter(comando);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            CloseConnection();
+            return ds.Tables[0];
+        }
+        else return new DataTable();
+    }
+
+    public DataTable Query(string sql, SqlParameter parametro = null)
+    {
+        if (OpenConnection())
+        {
+            SqlCommand comando = new SqlCommand(sql, Connection);
+            comando.CommandType = CommandType.Text;
+
+            if (parametro != null)
+                comando.Parameters.Add(parametro);
+
+
+
+            SqlDataAdapter da = new SqlDataAdapter(comando);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            CloseConnection();
+            return ds.Tables[0];
+        }
+        else return new DataTable();
+    }
+}
